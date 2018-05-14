cbd617583fc01743a84cd5c3f72a6bb506d522ec
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-05-14 16:39:55 -0500

    set up routes;

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Auth from "./component/Auth/Auth";
 import Dashboard from "./component/Dashboard/Dashboard";
 import Form from "./component/Form/Form";
 import Post from "./component/Post/Post";
+import { withRouter } from "react-router-dom";
+
+import routes from "./routes";
 
 class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="App">
-        <Nav />
-        <Auth />
-        <Dashboard />
-        <Form />
-        <Post />
+        {this.props.location.pathname === "/" ? "" : <Nav />}
+
+        {routes}
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <Link to="/dashboard">
+        <button>Home</button>
+      </Link>
+      <Link to="/post/:postid">
+        <button>New Post</button>
+      </Link>
+      <Link to="/">
+        <button>Logout</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Nav;
