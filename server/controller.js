e41ed63987f59ba174c9a87c52aaa09223128b73
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-05-15 13:30:19 -0500

    completed post endpoint

--- server/controller.js
+++ server/controller.js
@@ -3,6 +3,11 @@ module.exports = {
     const { username, password } = req.body;
     const { dbInstance } = req.app.get("db");
 
-    dbInstance.create_new_user([userName, password]).then(;
+    dbInstance
+      .create_new_user([userName, password])
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(console.log);
   }
 };
--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 
+const authPage = {
+  background: "linear-gradient(to right, #ff70a6, #e9ff70",
+  height: "100vh"
+};
+
+const authContainer = {
+  height: "50vh",
+  display: "flex",
+  flexDirection: "column",
+  color: "#f2f2f2",
+  width: "40vw"
+};
+
 class Auth extends Component {
   constructor() {
     super();
@@ -17,19 +30,23 @@ class Auth extends Component {
 
   render() {
     return (
-      <div>
-        <input
-          name="userName"
-          value={this.state.userName}
-          onChange={e => this.handleInput(e)}
-        />
-        <input
-          name="passWord"
-          value={this.state.passWord}
-          onChange={e => this.handleInput(e)}
-        />
-        <button>Login</button>
-        <button>Register</button>
+      <div style={authPage}>
+        <div style={authContainer}>
+          <img />
+          <h1>Helo</h1>
+          <input
+            name="userName"
+            value={this.state.userName}
+            onChange={e => this.handleInput(e)}
+          />
+          <input
+            name="passWord"
+            value={this.state.passWord}
+            onChange={e => this.handleInput(e)}
+          />
+          <button>Login</button>
+          <button>Register</button>
+        </div>
       </div>
     );
   }
